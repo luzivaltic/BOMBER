47bee9ec09443c1471bc4d2172f65ab7289bb85b
unknown <gokuhdvn3@gmail.com>
2022-10-26 15:46:35 +0700

    something

--- src/main/java/Bomber/Game.java
+++ src/main/java/Bomber/Game.java
@@ -28,60 +28,6 @@ public class Game extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, Color.BLACK );
-        Text text = new Text();
-        text.setText("Hello Worlds");
-        text.setX(55);
-        text.setY(55);
-        text.setFont( Font.font("Aria" , 50) );
-        text.setFill(Color.MIDNIGHTBLUE);
 
-        // Effect
-        Glow glow = new Glow();
-        glow.setLevel(0.5);
-
-        Image background = new Image("galaxy.jpg");
-        ImageView backImage = new ImageView(background);
-        backImage.setX(50);
-        backImage.setY(50);
-        backImage.setFitHeight(100);
-        backImage.setPreserveRatio(true);
-        backImage.setEffect(glow);
-
-        Circle circle = new Circle();
-        circle.setCenterX(200);
-        circle.setCenterY(200);
-        circle.setRadius(50);
-        circle.setFill( Color.CRIMSON );
-
-        EventHandler<MouseEvent> filterEvent = new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                System.out.println("HE ok easy");
-                circle.setFill(Color.YELLOW);
-            }
-        };
-
-        EventHandler<MouseEvent> handlerEvent = new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                System.out.println("HE ok easy");
-                circle.setFill(Color.RED);
-                System.out.println("TESTER");
-            }
-        };
-
-
-        circle.addEventFilter(MouseEvent.MOUSE_CLICKED , filterEvent);
-        circle.addEventHandler(MouseEvent.MOUSE_CLICKED , handlerEvent );
-
-        root.getChildren().add(circle);
-        root.getChildren().add(backImage);
-        root.getChildren().add(text);
-        stage.getIcons().add(background);
-        stage.setTitle("BomberMan");
-        stage.setScene(scene);
-        stage.show();
     }
 }
