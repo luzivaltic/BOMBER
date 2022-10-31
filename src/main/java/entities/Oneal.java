eb07131463b14189a8c33079a4d67b18c398d164
natalieconan <natalieconan17@gmail.com>
2022-10-31 16:05:02 +0700

    aa

--- src/main/java/entities/Oneal.java
+++ src/main/java/entities/Oneal.java
@@ -5,7 +5,18 @@ import javafx.scene.image.Image;
 
 import Bomber.Game;
 
+import java.util.*;
+
 import static Bomber.Game.*;
+
+class Pair {
+    public int x;
+    public int y;
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 public class Oneal extends Monster {
     private long IntervalChangeDirection = 2100000000;
@@ -28,55 +39,85 @@ public class Oneal extends Monster {
         spriteChange();
     }
 
-    private boolean checkGrid(int x, int y) {
-        if (x < 0 || y < 0 || x >= WIDTH * Sprite.SCALED_SIZE || y >= HEIGHT * Sprite.SCALED_SIZE) {
+    private boolean checkGrid(int u, int v) {
+        /*if (u < 0 || v < 0 || u >= WIDTH || v >= HEIGHT) {
             return false;
         }
 
-        // check if it is brick
+        Entity temp = new Monster(u * Sprite.SCALED_SIZE, v * Sprite.SCALED_SIZE, null);
+
+        for (Entity entity : stillObjects) {
+            if (entity instanceof Wall) {
+                if (temp.isCollide(entity)) {
+                    return false;
+                }
+            }
+        }
+
+        for (Entity entity : entities) {
+            if (entity instanceof Brick) {
+                if (temp.isCollide(entity)) {
+                    return false;
+                }
+            }
+        }*/
 
         return true;
     }
 
-    void dfs(int[][] distance, int x, int y) {
-        for (int dir = 0; dir < 4; ++dir) {
-            int u = x + DIR_X[dir] * Sprite.SCALED_SIZE;
-            int v = y + DIR_Y[dir] * Sprite.SCALED_SIZE;
+    private void findBomber() {
+        /*System.err.println(bomber.x + " " + bomber.y);
 
-            if (checkGrid(u, v) && distance[u][v] == 0) {
-                distance[u][v] = distance[x][y] + 1;
-                dfs(distance, u, v);
+        LinkedList<Pair> queue = new LinkedList<>();
+        boolean[][] color = new boolean[WIDTH][HEIGHT];
+        int[][] distance = new int[WIDTH][HEIGHT];
+
+        queue.add(new Pair(convert(bomber.x), convert(bomber.y)));
+        distance[convert(bomber.x)][convert(bomber.y)] = 1;
+        color[convert(bomber.x)][convert(bomber.y)] = true;
+
+        while (queue.isEmpty() == false) {
+            Pair element = queue.getFirst();
+            queue.removeFirst();
+
+            int x = element.x;
+            int y = element.y;
+
+            for (int dir = 0; dir < 4; ++dir) {
+                int u = x + DIR_X[dir];
+                int v = y + DIR_Y[dir];
+
+                if (checkGrid(u, v) == true) {
+                    if (color[u][v] == false) {
+                        color[u][v] = true;
+                        distance[u][v] = distance[x][y] + 1;
+                        queue.add(new Pair(u, v));
+                    }
+                }
             }
         }
-    }
 
-    private void findBomber() {
-        int[][] distance = new int[WIDTH * Sprite.SCALED_SIZE][HEIGHT * Sprite.SCALED_SIZE];
-        distance[bomber.x][bomber.y] = 2;
+        System.err.println(distance[convert(this.x)][convert(this.y)]);
 
-        dfs(distance, bomber.x, bomber.y);
-
-        if (distance[x][y] != 0) {
+        if (distance[convert(x)][convert(y)] != 0) {
             for (int dir = 0; dir < 4; ++dir) {
-                int u = x + DIR_X[dir] * Sprite.SCALED_SIZE;
-                int v = y + DIR_Y[dir] * Sprite.SCALED_SIZE;
-
-                if (checkGrid(u, v) == true && distance[x][y] == distance[u][v] + 1) {
+                int u = convert(x) + DIR_X[dir];
+                int v = convert(y) + DIR_Y[dir];
+                if (checkGrid(u, v) == true && distance[convert(x)][convert(y)] == distance[u][v] + 1) {
                     direct = dir;
                     break;
                 }
             }
         } else {
             for (int dir = 0; dir < 4; ++dir) {
-                int u = x + DIR_X[dir] * Sprite.SCALED_SIZE;
-                int v = y + DIR_Y[dir] * Sprite.SCALED_SIZE;
-
-                if (checkGrid(u, v)) {
+                int u = convert(x) + DIR_X[dir];
+                int v = convert(y) + DIR_Y[dir];
+                if (checkGrid(u, v) == true) {
                     direct = dir;
                     break;
                 }
             }
-        }
+        }*/
     }
 
     public void move() {
